b64b35e9eb6df571d27c2bd9557916a44d16c667
W5oAmI <ohassan864@gmail.com>
2023-04-12 23:44:08 +0000

    séance 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!'
+  serverName = "Server one";
+  username = "";
 
+  constructor() {
+    setTimeout( ()=>{
+      this.allowNewServer = true;
+    },2000)
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created!' + this.serverName
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement> event.target).value;
+  }
+
+  onReset(){
+    this.username = '';
+  }
 }
